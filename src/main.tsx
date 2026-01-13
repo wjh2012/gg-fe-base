6f521c410d2ecfbb2b9b39a465b40de5b2a0b6a9
wjh <wjh2012@naver.com>
2026-01-13 13:44:22 +0900

    fix: pretendard import 수정

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "@fontsource/pretendard/index.css";
 import "@fontsource/pretendard/400.css";
-import "@fontsource/pretendard/400-italic.css";
 import "./index.css";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "@/lib/query-client.ts";
