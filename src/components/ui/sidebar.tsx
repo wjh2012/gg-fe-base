b0ba07a474c1057cc03d743092769483b12f7dae
wjh <wjh2012@naver.com>
2026-01-13 14:13:37 +0900

    style: 사이드바가 컨텐츠를 밀어내지 않도록 수정

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -218,12 +218,8 @@ function Sidebar({
       <div
         data-slot="sidebar-gap"
         className={cn(
-          "relative w-(--sidebar-width) bg-transparent transition-[width] duration-200 ease-linear",
-          "group-data-[collapsible=offcanvas]:w-0",
+          "relative w-0 bg-transparent transition-[width] duration-200 ease-linear",
           "group-data-[side=right]:rotate-180",
-          variant === "floating" || variant === "inset"
-            ? "group-data-[collapsible=icon]:w-[calc(var(--sidebar-width-icon)+(--spacing(4)))]"
-            : "group-data-[collapsible=icon]:w-(--sidebar-width-icon)",
         )}
       />
       <div
