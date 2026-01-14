c5d5485beab10249a9df87a7d68fc97be6f05589
wjh <wjh2012@naver.com>
2026-01-14 13:08:17 +0900

    style: 새로고침시 폰트 변화 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "@fontsource/pretendard/index.css";
-import "@fontsource/pretendard/400.css";
 import "./index.css";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "@/lib/query-client.ts";
